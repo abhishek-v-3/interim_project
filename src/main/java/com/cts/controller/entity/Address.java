07398f6a5d7802af45ed0c39ec71025fdc8ec573
Abhishek V <2370829@cognizant.com>
2024-12-10 18:25:06 +0530

    added attributes to Patient

---
+++ src/main/java/com/cts/controller/entity/Address.java
@@ -0,0 +1,19 @@
+package com.cts.controller.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+@Embeddable
+@Data
+public class Address {
+
+	private String houseName;
+	private String area;
+	private String streetName;
+	private String landMark;
+	private String state;
+	private String district;
+	private String country;
+	private String pincode;
+	
+}
--- src/main/java/com/cts/controller/entity/Patient.java
+++ src/main/java/com/cts/controller/entity/Patient.java
@@ -1,5 +1,25 @@
 package com.cts.controller.entity;
 
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Patient {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private int age;
+	private String gender;
+	
+	@Embedded
+	Address address;
+	
+	
 }
